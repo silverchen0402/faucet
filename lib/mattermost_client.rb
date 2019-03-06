db8c43966e69abb527ae5b5b0ff664cfd64c31fe
Valdemar <vavanx@gmail.com>
2019-03-06 11:27:30 +0200

    mattermost metadata

--- lib/mattermost_client.rb
+++ lib/mattermost_client.rb
@@ -8,10 +8,12 @@ class MattermostClient
 
   def self.notify(text)
     url = Rails.application.config.faucet.mattermost_hook
+    username = Rails.application.config.faucet.mattermost_username
+    channel  = Rails.application.config.faucet.mattermost_channel
     uri = URI(url)
     req = Net::HTTP::Post.new(uri)
 
-    req.body = 'payload=' + { text: text, username: 'faucet', channel: 'faucet' }.to_json
+    req.body = 'payload=' + { text: text, username: username, channel: channel }.to_json
     res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
       http.request(req)
     end
